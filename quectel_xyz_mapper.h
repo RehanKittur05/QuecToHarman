85b9cd3a396039a2595541ded8ecb5d0e6520b29
rkittur <rehanayub.kittur@harman.com>
2025-04-04 06:26:17 +0000

    changes made in main function.

--- quectel_xyz_mapper.h
+++ quectel_xyz_mapper.h
@@ -12,18 +12,36 @@ enum QuectelState {
     BACKUP,
     CANCEL
 };
+std::string success_done(){
+ return "Success";
+}
+std::string inactive_update(){
+    return "INACTIVE UPDATE";
+}
+std::string improper_write(){
+    return "IMPROPER WRITE";
+}
+std::string sync_in_progress(){
+    return "SYNC IN PROGRESS";
+}
+std::string perform_backup(){
+    return "PERFORMING BACKUP";
+}
+std::string cancel(){
+    return "Cancel";
+}
 
 // Mapper class that maps Quectel states to XYZ strings
 class QuectelToXYZMapper {
 public:
     std::string getXYZState(QuectelState state){
         switch (state) {
-            case SUCCEED: return "Success";
-            case UPDATE: return "Inactive_update";
-            case WRITEDONE: return "Improper_write";
-            case SYNC_PENDING: return "sync_in_progress";
-            case BACKUP: return "performing_backup";
-            case CANCEL: return "cancel";
+            case SUCCEED: return success_done();
+            case UPDATE: return inactive_update();
+            case WRITEDONE: return improper_write();
+            case SYNC_PENDING: return sync_in_progress();
+            case BACKUP: return perform_backup();
+            case CANCEL: return cancel();
             // default: return "Invalid State";
         }
     }
